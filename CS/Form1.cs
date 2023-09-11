bae4334b1f38a2da728f206e66cb51cef5201687
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 17:15:10 +0400

    Change example status to out of maintenance

--- CS/Form1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DXApplication99 {
-    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
-        public Form1() {
-            InitializeComponent();
-            CustomViewInfoRegistrator viewInfoRegistrator = new CustomViewInfoRegistrator();
-            navBarControl1.View = viewInfoRegistrator;
-        }
-    }
-}
